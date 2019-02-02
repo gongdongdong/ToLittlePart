828010c02a010d30778bfc65acaa57b2cd98f36f
GongDongdong <gdd330282@163.com>
2019-02-02 12:31:02 +0800

    dependency inject failed case commit once

--- app/src/main/java/com/gdd/tolittlepart/MainActivity.java
+++ app/src/main/java/com/gdd/tolittlepart/MainActivity.java
@@ -2,12 +2,20 @@ package com.gdd.tolittlepart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.gdd.tolittlepart.entity.StudentBean;
+
+import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Inject
+    StudentBean studentBean;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("tag", studentBean.get_id() + studentBean.getName());
     }
 }
---
+++ app/src/main/java/com/gdd/tolittlepart/entity/StudentBean.java
@@ -0,0 +1,30 @@
+package com.gdd.tolittlepart.entity;
+
+import javax.inject.Inject;
+
+public class StudentBean {
+
+    @Inject
+    public StudentBean(StudentBean studentBean){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    private String name;
+    private String _id;
+}
