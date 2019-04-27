100ae0f62835731fe2740ab0392e9bf7e4eae79f
GongDongdong <gdd330282@163.com>
2019-04-27 22:43:25 +0800

    test ssh

--- app/src/main/java/com/gdd/tolittlepart/apps/MyApplication.java
+++ app/src/main/java/com/gdd/tolittlepart/apps/MyApplication.java
@@ -1,9 +1,5 @@
 package com.gdd.tolittlepart.apps;
 
-import android.app.Application;
-
-import com.alibaba.android.arouter.BuildConfig;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.gdd.base.component.BaseApplication;
 
 public class MyApplication extends BaseApplication {
