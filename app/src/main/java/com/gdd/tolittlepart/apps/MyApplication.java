dd33d196a141e03e81eae6c2c208e07b23b9f4f4
GongDongdong <gdd330282@163.com>
2019-04-27 18:48:20 +0800

    test config version

--- app/src/main/java/com/gdd/tolittlepart/apps/MyApplication.java
+++ app/src/main/java/com/gdd/tolittlepart/apps/MyApplication.java
@@ -1,12 +1,20 @@
 package com.gdd.tolittlepart.apps;
 
-import com.gdd.base.BuildConfig;
+import android.app.Application;
+
+import com.alibaba.android.arouter.BuildConfig;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.gdd.base.component.BaseApplication;
 
-public class MyApplication extends BaseApplication {
+public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        if (BuildConfig.DEBUG) {           // These two lines must be written before init, otherwise these configurations will be invalid in the init process
+            ARouter.openLog();     // Print log
+            ARouter.openDebug();   // Turn on debugging mode (If you are running in InstantRun mode, you must turn on debug mode! Online version needs to be closed, otherwise there is a security risk)
+        }
+        ARouter.init(this); // As early as possible, it is recommended to initialize in the Application
     }
 }
--- base/src/main/java/com/gdd/base/component/BaseApplication.java
+++ base/src/main/java/com/gdd/base/component/BaseApplication.java
@@ -2,28 +2,17 @@ package com.gdd.base.component;
 
 import android.app.Application;
 
-import com.alibaba.android.arouter.launcher.ARouter;
-import com.gdd.base.BuildConfig;
+public class BaseApplication extends Application {
 
-public abstract class BaseApplication extends Application {
-
-    public Application getAppInstance() {
-        return appInstance;
-    }
-
-    private Application appInstance;
+    BaseApplication appInstance;
     @Override
     public void onCreate() {
         super.onCreate();
         appInstance = this;
-        if (isDebug()) {           // These two lines must be written before init, otherwise these configurations will be invalid in the init process
-            ARouter.openLog();     // Print log
-            ARouter.openDebug();   // Turn on debugging mode (If you are running in InstantRun mode, you must turn on debug mode! Online version needs to be closed, otherwise there is a security risk)
-        }
-        ARouter.init(appInstance); // As early as possible, it is recommended to initialize in the Application
-    }
-
-    private boolean isDebug() {
-        return BuildConfig.DEBUG;
+//        if (BuildConfig.DEBUG) {           // These two lines must be written before init, otherwise these configurations will be invalid in the init process
+//            ARouter.openLog();     // Print log
+//            ARouter.openDebug();   // Turn on debugging mode (If you are running in InstantRun mode, you must turn on debug mode! Online version needs to be closed, otherwise there is a security risk)
+//        }
+//        ARouter.init(appInstance); // As early as possible, it is recommended to initialize in the Application
     }
 }
