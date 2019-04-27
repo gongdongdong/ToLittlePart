c05aca8d0a358062f740cde1c0ab6ddcf05fc685
GongDongdong <gdd330282@163.com>
2019-04-27 21:59:04 +0800

    add arouter demo

--- app/src/main/java/com/gdd/tolittlepart/views/splash/SplashActivity.java
+++ app/src/main/java/com/gdd/tolittlepart/views/splash/SplashActivity.java
@@ -2,18 +2,21 @@ package com.gdd.tolittlepart.views.splash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.gdd.tolittlepart.R;
+import com.gdd.useraccountapi.loginService.LoginService;
 
+@Route(path = "/tolittle/splash")
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash2);
-        findViewById(R.id.btn_check_arouter).setOnClickListener(view->{
-            ARouter.getInstance().build("/tolittle/main").navigation();
-        });
+        LoginService loginService = (LoginService) ARouter.getInstance().build("/tolittle/hello").navigation();
+        Log.e("test", loginService.sayHello("gdd"));
     }
 }
