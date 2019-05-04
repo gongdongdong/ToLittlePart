f0e9ea380c25ffe830601b01f078cc71e5e08f8d
GongDongdong <gdd330282@163.com>
2019-05-04 18:42:15 +0800

    add the viewpager and the coresponding navibar

---
+++ app/src/main/java/com/gdd/tolittlepart/Adapters/MyViewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.gdd.tolittlepart.Adapters;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.gdd.tolittlepart.views.main.newsfragment.NewsFragment;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MyViewPagerAdapter extends FragmentPagerAdapter {
+
+
+    Map<String, Fragment> mFragments = new HashMap<>();
+    List<String> keywords = new LinkedList<>();
+    public MyViewPagerAdapter(FragmentManager fm, List<String> keywords) {
+        super(fm);
+        this.keywords = keywords;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        if(mFragments.get(keywords.get(i)) != null){
+            return mFragments.get(keywords.get(i));
+        }
+
+        Fragment oneFragment = NewsFragment.getInstance();
+        Bundle bundle = new Bundle();
+        bundle.clear();
+        bundle.putString("keyword", keywords.get(i));
+        oneFragment.setArguments(bundle);
+        mFragments.put(keywords.get(i), oneFragment);
+        return oneFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return keywords.size();
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return keywords.get(position);
+    }
+}
--- app/src/main/java/com/gdd/tolittlepart/views/main/MainActivity.java
+++ app/src/main/java/com/gdd/tolittlepart/views/main/MainActivity.java
@@ -2,15 +2,20 @@ package com.gdd.tolittlepart.views.main;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.gdd.base.adapter.listview.CommonAdapter;
+import com.gdd.tolittlepart.Adapters.MyViewPagerAdapter;
 import com.gdd.tolittlepart.aop.TestAnnoTrace;
 import com.gdd.base.component.BaseActivity;
 import com.gdd.events.NetworkEvent;
@@ -21,20 +26,31 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import butterknife.BindView;
 
 @Route(path = "/tolittle/main")
 public class MainActivity extends BaseActivity {
 
     private static final String TAG = "test";
-    @BindView(R.id.fl_show_fragment)
-    FrameLayout fl_show_fragment;
+//    @BindView(R.id.fl_show_fragment)
+//    FrameLayout fl_show_fragment;
     @BindView(R.id.dl_dlcontent)
     DrawerLayout dl_dlcontent;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.vp_show_content)
+    ViewPager vp_show_content;
+    @BindView(R.id.tabs)
+    TabLayout mtabs;
+    @BindView(R.id.add_channel_iv)
+    ImageView add_channel_iv;
 
     CommonAdapter<String> commonAdapter;
+
+    MyViewPagerAdapter myViewPagerAdapter;
 
     NewsFragment newsFragment;
     FragmentManager fragmentManager;
@@ -50,20 +66,20 @@ public class MainActivity extends BaseActivity {
     protected void initData() {
 
         setSupportActionBar(toolbar);
-        if(newsFragment == null){
-            newsFragment = NewsFragment.getInstance();
-        }
-        fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction;
-        if(newsFragment.isAdded()){
-            fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.show(newsFragment);
-        }
-        else{
-            fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.fl_show_fragment, newsFragment);
-        }
-        fragmentTransaction.commit();
+//        if(newsFragment == null){
+//            newsFragment = NewsFragment.getInstance();
+//        }
+//        fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction;
+//        if(newsFragment.isAdded()){
+//            fragmentTransaction = fragmentManager.beginTransaction();
+//            fragmentTransaction.show(newsFragment);
+//        }
+//        else{
+//            fragmentTransaction = fragmentManager.beginTransaction();
+//            fragmentTransaction.add(R.id.fl_show_fragment, newsFragment);
+//        }
+//        fragmentTransaction.commit();
 
 //        List<String> mylist = new LinkedList<>();
 //        for(String one : myarray){
@@ -77,6 +93,51 @@ public class MainActivity extends BaseActivity {
 //        };
 //
 //        lv_test_common.setAdapter(commonAdapter);
+        fragmentManager = getSupportFragmentManager();
+        List<String> myKeys = new LinkedList<>();
+        myKeys.add("one");
+        myKeys.add("two");
+        myKeys.add("three");
+        myKeys.add("four");
+        myKeys.add("five");
+        myKeys.add("six");
+        myKeys.add("senven");
+        myKeys.add("eight");
+        myKeys.add("nine");
+        myKeys.add("ten");
+        myViewPagerAdapter = new MyViewPagerAdapter(fragmentManager, myKeys);
+        vp_show_content.setAdapter(myViewPagerAdapter);
+        vp_show_content.setCurrentItem(0);
+        mtabs.setupWithViewPager(vp_show_content);
+        dynamicSetTabLayoutMode(mtabs);
+        add_channel_iv.setOnClickListener((view)->{
+            Log.e(TAG, "ADD IMAGE CLICKED");
+        });
+    }
+
+    public void dynamicSetTabLayoutMode(TabLayout tabLayout) {
+        int tabWidth = calculateTabWidth(tabLayout);
+        int screenWidth = getScreenWith();
+
+        if (tabWidth <= screenWidth) {
+            tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        } else {
+            tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        }
+    }
+
+    public int getScreenWith() {
+        return getResources().getDisplayMetrics().widthPixels;
+    }
+
+    private int calculateTabWidth(TabLayout tabLayout) {
+        int tabWidth = 0;
+        for (int i = 0; i < tabLayout.getChildCount(); i++) {
+            final View view = tabLayout.getChildAt(i);
+            view.measure(0, 0); // 通知父view测量，以便于能够保证获取到宽高
+            tabWidth += view.getMeasuredWidth();
+        }
+        return tabWidth;
     }
 
     @Override
